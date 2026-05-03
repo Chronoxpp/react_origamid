4cb285d066b428132585eb80cd117f20a0074885
Lucas Dourado Rodrigues <lucasdouradochrono@gmail.com>
2026-05-03 14:18:26 -0400

    Feat: implement o desafio da aula 0401 inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,145 @@
-// Utilize o GlobalContext do exemplo anterior para puxar os dados da API abaixo:
-// https://ranekapi.origamid.dev/json/api/produto/
-// assim que o usuário acessar o app
-// coloque os dados da API no contexto global, dando acesso aos dados da mesma
-// defina uma função chamada limparDados que é responsável por zerar os dados de produto
-// e exponha essa função no contexto global
+// Faça um fetch (POST) para a API abaixo
+// Para a criação ser aceita é necessário enviar dodos de:
+// nome, email, senha, cep, rua, numero, bairro, cidade e estado
+
+// // Essa é a função utilizado para realizar o POST
+// fetch('https://ranekapi.origamid.dev/json/api/usuario', {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+//   // form é o objeto com os dados do formulário
+//   body: JSON.stringify(form),
+// });
+
+// Mostre uma mensagem na tela, caso a resposta da API seja positiva
 import React from "react";
-import { GlobalContext } from "./GlobalContext";
 
 export default function App()
 {
-  const globalContext = React.useContext(GlobalContext);
-
-  const api = "https://ranekapi.origamid.dev/json/api/produto/";
-
-  salvarProdutosEmContexto(api);
+  const [nome, setNome] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [senha, setSenha] = React.useState('');
+  const [cep, setCep] = React.useState('');
+  const [rua, setRua] = React.useState('');
+  const [numero, setNumero] = React.useState('');
+  const [bairro, setBairro] = React.useState('');
+  const [cidade, setCidade] = React.useState('');
+  const [estado, setEstado] = React.useState('');
+  
+  const [usuarioFoiCadastrado, setUsuarioFoiCadastrado] = React.useState(false);
 
   return (
     <>
-      <h1>App</h1>
+      <h1>Cadastro de usuário</h1>
+
+      <form onSubmit={cadastrarUsuario}>
+        <label htmlFor="nome">Nome</label>
+        <input 
+          type="text" 
+          id="nome" 
+          value={nome} 
+          onChange={event => setNome(event.target.value)} 
+        />
+
+        <label htmlFor="email">Email</label>
+        <input 
+          type="email" 
+          id="email"
+          value={email} 
+          onChange={event => setEmail(event.target.value)}
+        />
+
+        <label htmlFor="senha">Senha</label>
+        <input 
+          type="password" 
+          id="senha"
+          value={senha} 
+          onChange={event => setSenha(event.target.value)}  
+        />
+        
+        <label htmlFor="cep">CEP</label>
+        <input 
+          type="text" 
+          id="cep" 
+          value={cep}
+          onChange={event => setCep(event.target.value)}
+        />
+
+        <label htmlFor="rua">Rua</label>
+        <input 
+          type="text" 
+          id="rua" 
+          value={rua}
+          onChange={event => setRua(event.target.value)}
+        />
+
+        <label htmlFor="numero">Numero</label>
+        <input 
+          type="text" 
+          id="numero" 
+          value={numero}
+          onChange={event => setNumero(event.target.value)}
+        />
+
+        <label htmlFor="bairro">Bairro</label>
+        <input 
+          type="text" 
+          id="bairro" 
+          value={bairro}
+          onChange={event => setBairro(event.target.value)}
+        />
+
+        <label htmlFor="cidade">Cidade</label>
+        <input 
+          type="text" 
+          id="cidade" 
+          value={cidade}
+          onChange={event => setCidade(event.target.value)}
+        />
+
+        <label htmlFor="estado">Estado</label>
+        <input 
+          type="text" 
+          id="estado" 
+          value={estado}
+          onChange={event => setEstado(event.target.value)}
+        />
+
+        <button>Cadastrar</button>
+        {usuarioFoiCadastrado && <p>Cadastrado realizado!</p>}
+      </form>
     </>
   );
 
-  async function salvarProdutosEmContexto(api)
+  async function cadastrarUsuario(event)
   {
-    const produtos = await obterProdutos(api);
+    event.preventDefault();
+    const form = {
+      nome: nome,
+      email: email,
+      senha: senha,
+      cep: cep,
+      rua: rua,
+      numero: numero,
+      bairro: bairro,
+      cidade: cidade,
+      estado: estado
+    };
 
-    globalContext.produtos = produtos;
-  }
+    const apiCadastroUsuario = 'https://ranekapi.origamid.dev/json/api/usuario';
 
-  async function obterProdutos(api)
-  {
-    const response = await fetch(api);
-    const produtos = await response.json();
+    const json = JSON.stringify(form);
 
-    return produtos
+    const response = await fetch(
+      apiCadastroUsuario,
+      {
+        method: "POST",
+        headers: {'Content-Type': 'application/json'},
+        body: json
+      }
+    );
+
+    if(response.ok) setUsuarioFoiCadastrado(true);
   }
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,9 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { GlobalStorage } from "./GlobalContext";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <GlobalStorage>
-            <App />
-        </GlobalStorage>
+        <App />
     </StrictMode>
 )
