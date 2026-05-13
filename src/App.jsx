54ce73ff651ad6ed616ff4456c2e984459478438
Lucas Dourado Rodrigues <lucasdouradochrono@gmail.com>
2026-05-13 17:07:08 -0400

    Feat: prepara os arquivos para o projeto do capitulo 8 do curso (projeto final)

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,8 @@
 import React from "react";
-import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Produtos from "./Components/Produtos";
-import Header from "./Components/Header";
-import Footer from "./Components/Footer";
-import Contato from "./Components/Contato";
-import Produto from "./Components/Produto";
 
 export default function App()
 {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Header />
-
-        <main className="Content">
-          <Routes>
-            <Route path="/" element={<Produtos />} />
-            <Route path="produto/:id" element={<Produto />} />
-            <Route path="contato" element={<Contato />} />
-          </Routes>
-        </main>
-
-        <Footer />
-      </BrowserRouter>
-    </div>
+    <div>App React</div>
   );
 }
