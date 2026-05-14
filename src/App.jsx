eae263326bb18731e07cba57991a29f1b4d10b18
Lucas Dourado Rodrigues <lucasdouradochrono@gmail.com>
2026-05-14 00:02:04 -0400

    Feat: fim da aula 0804

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 import React from "react";
+import "./App.css";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+import Home from "./components/Home.jsx";
+import {BrowserRouter, Route, Router, Routes} from "react-router-dom";
+import Login from "./components/login/Login.jsx";
 
 export default function App()
 {
   return (
-    <div>App React</div>
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes >
+          <Route path="/" element={<Home />}/>
+          <Route path="/login" element={<Login />}/>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./Header.module.css"
+import {Link} from "react-router-dom";
+
+export default function Header()
+{
+  return (
+    <header className={styles.Header}>
+      <nav className="Container">
+        <Link to="/">Home</Link>
+        <Link to="/login">Login / Criar</Link>
+      </nav>
+    </header>
+  );
+}
